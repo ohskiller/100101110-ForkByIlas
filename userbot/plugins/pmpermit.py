495017ed4865ce4285c281470fab888505dd1467
JustIlas69 <68425090+JustIlas69@users.noreply.github.com>
2020-07-19 14:18:35 +0200

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -62,10 +62,6 @@ if Var.PRIVATE_GROUP_ID is not None:
             if not pmpermit_sql.is_approved(chat.id):
                 if not chat.id in PM_WARNS:
                     pmpermit_sql.approve(chat.id, "outgoing")
-                    bruh = "**🆕 ᴜꜱᴇʀ ❕**"
-                    rko = await bot.send_message(event.chat_id, bruh)
-                    await asyncio.sleep(3)
-                    await rko.delete()
 
 
     @command(pattern="^.block ?(.*)")
