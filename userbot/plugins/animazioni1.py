8798818b59b8fd10803ee52cec8f7170a4108332
JustIlas69 <68425090+JustIlas69@users.noreply.github.com>
2020-08-17 10:47:31 +0200

    Update animazioni1.py

--- userbot/plugins/animazioni1.py
+++ userbot/plugins/animazioni1.py
@@ -1,13 +1,3 @@
-# Copyright © 2020 di 100101110 Github, <https://github.com/100101110>.
-#
-# Questo file fa parte del progetto <https://github.com/100101110/userbot-100101110>,
-# e viene rilasciato in base alla "Licenza GNU Affero General Public v3.0".
-# Si prega di consultare <https://github.com/100101110/userbot-100101110/blob/master/LICENSE>
-#
-# Tutti i diritti riservati.
-# 
-# Crediti: @100101110
-#
 """Commands:
 .hypno
 .plane
