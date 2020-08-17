a0c36d2d3f6dbd2613ffb7abdaa74eb4fc9afdee
JustIlas69 <68425090+JustIlas69@users.noreply.github.com>
2020-08-17 11:24:28 +0200

    Create orologio.py

---
+++ userbot/plugins/orologio.py
@@ -0,0 +1,15 @@
+from telethon import events
+import asyncio
+from collections import deque
+from userbot.system import dev_cmd
+
+
+@borg.on(admin_cmd(pattern=r"clock"))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🕛🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚"))
+	for _ in range(60):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
