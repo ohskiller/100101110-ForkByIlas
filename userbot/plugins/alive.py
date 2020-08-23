9f94c0b48fe99982e68ab27643fc4f3b2b81605c
JustIlas69 <68425090+JustIlas69@users.noreply.github.com>
2020-08-23 23:21:21 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,10 +12,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Ilas"
 @bot.on(dev_cmd(pattern=f"alive", outgoing=True))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running. """
-    await alive.edit("ᡕᠵ᠊ᡃ່࡚ࠢ࠘ ᠊߯᠆ࠣ࠘ᡁࠣ࠘᠊᠊ࠢ࠘𐡏 **SISTEMA**\n"
+    await alive.edit("**SISTEMA ᕙ(  • ‿ •  )ᕗ**\n"
                      f"**> Telethon:** {version.__version__}\n"
                      f"**> Python:** {versions.__python_version__}\n"
                      f"**> Firmware:** {versions.__version__}\n"
                      f"**> Licenza:** {versions.__license__}\n"
-                     f"**> Copyright:** Ilas the gay\n"
                      f"›")
